c5f00969a22c30b651e264d55bc2810197d3ac19
Imran Sahil <imdshizzle@gmail.com>
2024-12-28 01:01:31 -0500

    test-example

---
+++ playwright/tests/test-example.spec.ts
@@ -0,0 +1,27 @@
+import { expect, test } from "playwright/test";
+
+test.describe("Dummy Test Suite", () => {
+  test("Google Test Pass", async ({ page }) => {
+    await page.goto("https://www.google.com/");
+    // setTimeout(() => {}, 3000);
+    expect(await page.title()).toBe("Google");
+  });
+
+  test("Google Test Fail", async ({ page }) => {
+    await page.goto("https://www.apple.com/");
+    expect(await page.title()).toBe("Google");
+  });
+
+  const numbers = [1,2,3];
+  test("Test Random 1", async () => {
+    expect(numbers[Math.floor(Math.random() * numbers.length)]).toBe(1);
+  })
+
+  test("Test Random 2", async () => {
+    expect(numbers[Math.floor(Math.random() * numbers.length)]).toBe(2);
+  });
+
+  test.skip("Skipped Test", async () => {
+    expect(true).toBe(false);
+  });
+});
