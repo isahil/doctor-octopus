054c5a7e8e8c13c66d8e597dfdaaf43ccea0a135
Imran Sahil <imdshizzle@gmail.com>
2025-03-05 23:11:51 -0500

    runner S3 path updates

--- playwright/utils/runner.js
+++ playwright/utils/runner.js
@@ -9,15 +9,16 @@ import {
   upload_directory,
 } from "./index.js";
 
-const { AWS_SDET_BUCKET_NAME, ENVIRONMENT, PRODUCT_TYPE, APP } = process.env;
+const { AWS_SDET_BUCKET_NAME, ENVIRONMENT, APP } = process.env;
 const test_script_name = process.argv[2];
 const test_protocol = test_script_name.split(":")[0];
+const app = APP ?? "loan";
 
 const test_reports_dir = "test_reports";
 const report_dir = `${get_est_date_time()}`;
 const local_test_reports_dir = `./${test_reports_dir}/${report_dir}`;
 process.env.TEST_REPORTS_DIR = local_test_reports_dir;
-const s3_test_reports_dir = `trading-apps/${test_reports_dir}/${test_protocol}/${report_dir}`;
+const s3_test_reports_dir = `trading-apps/${test_reports_dir}/${APP}/${test_protocol}/${ENVIRONMENT}/${report_dir}`;
 
 const os_username = os.userInfo().username;
 const git_branch = execSync("git rev-parse --abbrev-ref HEAD")
@@ -32,13 +33,13 @@ const upload_report = async (code) => {
   report_card["stats"]["git_branch"] = git_branch;
   report_card["stats"]["username"] = os_username;
   report_card["stats"]["environment"] = ENVIRONMENT;
-  report_card["stats"]["app"] = PRODUCT_TYPE ? PRODUCT_TYPE : APP;
+  report_card["stats"]["app"] = app;
   report_card["stats"]["test_suite"] = test_script_name;
 
   // Write the updated reportCard object back to the report.json file
   fs.writeFileSync(json_report, JSON.stringify(report_card, null, 2));
   console.log(`Uploading test reports to S3 bucket: ${AWS_SDET_BUCKET_NAME}`);
-  if (CI)
+  // if (CI)
     await upload_directory(
       AWS_SDET_BUCKET_NAME,
       local_test_reports_dir,
