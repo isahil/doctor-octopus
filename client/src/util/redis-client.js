38a0b87d252fa5ca15d279e0e7786b83ce894f26
Imran Sahil <imdshizzle@gmail.com>
2025-06-02 07:51:44 -0400

    redis client update

--- client/src/util/redis-client.js
+++ client/src/util/redis-client.js
@@ -1,61 +1,57 @@
 import { createClient } from "redis"
-import config from "../config"
 
-const { REDIS_HOST, REDIS_PORT } = config
+// const { VITE_REDIS_HOST, VITE_REDIS_PORT } = process.env
 
-const client = createClient({
-  url: `redis://${REDIS_HOST}:${REDIS_PORT}`, // Update this URL to your Redis server URL if different
-})
-
-class Redis {
+export class Redis {
   constructor() {
-    this.connect()
-    this.client = client
+    this.client = this.connect()
   }
 
-  async connect() {
-    await client.connect()
+  connect() {
+    const client = createClient({
+      url: `redis://localhost:6379`, // Update this URL to your Redis server URL if different
+    })
+    client.connect()
+    return client
   }
 
-  async disconnect() {
-    await client.quit()
+  disconnect() {
+    client.quit()
   }
 
   async hl_push(key, field, data) {
-    const response = await client.hGet(key, field)
+    const response = await this.client.hGet(key, field)
     if (response) {
       const existing_list = JSON.parse(response)
       existing_list.push(data)
-      return await client.hSet(key, field, JSON.stringify(existing_list))
-    } else return await client.hSet(key, field, JSON.stringify([data]))
+      return await this.client.hSet(key, field, JSON.stringify(existing_list))
+    } else return await this.client.hSet(key, field, JSON.stringify([data]))
   }
 
   async hl_pop(key, field) {
-    const response = await client.hGet(key, field)
+    const response = await this.client.hGet(key, field)
     if (response) {
       const existing_list = JSON.parse(response)
       existing_list.pop()
-      return await client.hSet(key, field, JSON.stringify(existing_list))
+      return await this.client.hSet(key, field, JSON.stringify(existing_list))
     } else return null
   }
 
   async hl_shift(key, field) {
-    const response = await client.hGet(key, field)
+    const response = await this.client.hGet(key, field)
     if (response) {
       const existing_list = JSON.parse(response)
       existing_list.shift()
-      return await client.hSet(key, field, JSON.stringify(existing_list))
+      return await this.client.hSet(key, field, JSON.stringify(existing_list))
     } else return null
   }
 
   async hl_unshift(key, field, data) {
-    const response = await client.hGet(key, field)
+    const response = await this.client.hGet(key, field)
     if (response) {
       const existing_list = JSON.parse(response)
       existing_list.unshift(data)
-      return await client.hSet(key, field, JSON.stringify(existing_list))
-    } else return await client.hSet(key, field, JSON.stringify([data]))
+      return await this.client.hSet(key, field, JSON.stringify(existing_list))
+    } else return await this.client.hSet(key, field, JSON.stringify([data]))
   }
 }
-
-export default Redis
