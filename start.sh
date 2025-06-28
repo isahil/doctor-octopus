0ba48002693dfc41cb4a9679021a9e08541c2942
Imran Sahil <imdshizzle@gmail.com>
2025-06-28 15:12:12 -0400

    start.sh script updated for start-prod

--- start.sh
+++ start.sh
@@ -1,4 +1,6 @@
 #!bin/bash
 echo "Starting Background Process for Doctor Octopus App"
 nohup npm run start >> logs/doc.log & 2>&1 &
+nohup npm run notification >> logs/notification.log & 2>&1 &
+nohup npm run fixme >> logs/fixme.log & 2>&1 &
 echo "Background Process Started"
