712769b9b861a58cf732b7158f73013a817b1df9
Imran Sahil <imdshizzle@gmail.com>
2025-06-28 13:51:51 -0400

    Client S.S.E implementation for notifications

--- client/src/component/card/cards.jsx
+++ client/src/component/card/cards.jsx
@@ -1,14 +1,12 @@
 import { useEffect, useState } from "react"
 import Card from "./card"
 import "./cards.css"
-// import { useSocketIO } from "../../hooks"
 import Filters from "./filter"
 import config from "../../config.json"
 const { day_filter_conf, environment_filter_conf } = config
 const { VITE_SERVER_HOST, VITE_SERVER_PORT } = import.meta.env
 
 const Cards = () => {
-  // const { sio } = useSocketIO()
   const [cards, setCards] = useState([])
   const [totalCards, setTotalCards] = useState(0)
   const [isLoading, setIsLoading] = useState(true)
@@ -25,8 +23,33 @@ const Cards = () => {
     })
   }
 
+  const start_notification_stream = () => {
+    // Generate a client ID using current timestamp + random string. TODO: Use a more robust method for production.
+    const client_id = `${Date.now()}-${Math.random().toString(36).substring(2, 10)}`
+    const event_source = new EventSource(`${server_url}/notifications/${client_id}`)
+
+    event_source.onmessage = (event) => {
+      const data = JSON.parse(event.data)
+      // console.log("SSE notification received:", data)
+      if (data.type === "new_s3_objects") {
+        console.log(`New S3 objects alert: current ${data.count} (prev ${data.previous})`)
+        setAlert({ new: true, opening: false })
+        // get_cards_from_api() // Refresh Cards
+      }
+    }
+  }
+
+  /**
+   * Fetch cards data from the REST API endpoint.
+   * Clear the existing states before fetching new cards data.
+   * @returns {Promise<void>}
+   */
   const get_cards_from_api = async () => {
-    // Fetch cards data from the REST API endpoint
+    setIsLoading(true)
+    setCards([])
+    setTotalCards(0)
+    setAlert({ new: false, opening: false })
+
     const { source, day, environment } = filters
     const url = `${server_url}/cards/?source=${source}&day=${day}&environment=${environment}`
     const request = await fetch(url)
@@ -41,12 +64,8 @@ const Cards = () => {
   }
 
   useEffect(() => {
-    // clear the existing states before fetching new cards data
-    setIsLoading(true)
-    setCards([])
-    setTotalCards(0)
-    setAlert({ new: false, opening: false })
     get_cards_from_api()
+    start_notification_stream()
   }, [filters]) // fetch cards data when the source changes
 
   if (isLoading) {
