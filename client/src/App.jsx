337a8648a09f9bb538d64555ca0dc13a2c61443e
Imran Sahil <imdshizzle@gmail.com>
2025-08-10 01:56:21 -0400

    color design update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from "react"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import NavBar from "./component/navbar/navbar.jsx"
 import Cards from "./component/card/cards.jsx"
