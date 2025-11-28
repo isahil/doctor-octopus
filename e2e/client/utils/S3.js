899baba9330c91f6b5c70a5e1cfba1ed0a4abbda
Imran Sahil <imdshizzle@gmail.com>
2025-11-27 23:37:28 -0500

    env_loader.js and reporter.js files intro

--- e2e/client/utils/S3.js
+++ e2e/client/utils/S3.js
@@ -1,100 +1,83 @@
 import fs from "fs";
 import path from "path";
 import {
-  GetObjectCommand,
-  ListObjectsCommand,
-  PutObjectCommand,
-  S3Client,
+	GetObjectCommand,
+	ListObjectsCommand,
+	PutObjectCommand,
+	S3Client,
 } from "@aws-sdk/client-s3";
+import {
+	get_aws_sdet_bucket_name,
+	get_aws_sdet_bucket_region,
+	get_aws_sdet_bucket_access_key_id,
+	get_aws_sdet_bucket_secret_access_key,
+} from "./env_loader.js";
 
-const {
-  AWS_SDET_BUCKET_NAME: bucketName,
-  AWS_SDET_BUCKET_REGION: region,
-  AWS_SDET_BUCKET_ACCESS_KEY_ID: accessKeyId,
-  AWS_SDET_BUCKET_SECRET_ACCESS_KEY: secretAccessKeyId,
-} = process.env;
+const bucketName = get_aws_sdet_bucket_name();
+const region = get_aws_sdet_bucket_region();
+const accessKeyId = get_aws_sdet_bucket_access_key_id();
+const secretAccessKey = get_aws_sdet_bucket_secret_access_key();
 
 const S3_Client = new S3Client({
-  credentials: {
-    accessKeyId: accessKeyId,
-    secretAccessKey: secretAccessKeyId,
-  },
-  region: region,
+	credentials: { accessKeyId, secretAccessKey },
+	region,
 });
 
 export async function list_objects() {
-  const params = {
-    Bucket: "doctor-octopus",
-    Prefix: "test_reports/",
-  };
+	const params = {
+		Bucket: "doctor-octopus",
+		Prefix: "test_reports/",
+	};
 
-  const command = new ListObjectsCommand(params);
-  const response = await S3_Client.send(command);
-  return response;
+	const command = new ListObjectsCommand(params);
+	const response = await S3_Client.send(command);
+	return response;
 }
 
 export async function get_object(imageName) {
-  const params = {
-    Bucket: bucketName,
-    Key: imageName,
-  };
+	const params = {
+		Bucket: bucketName,
+		Key: imageName,
+	};
 
-  const command = new GetObjectCommand(params);
-  const response = await S3_Client.send(command);
-  // console.log(`Get S3 response ::: ${JSON.stringify(response)}`);
-  return response;
+	const command = new GetObjectCommand(params);
+	const response = await S3_Client.send(command);
+	// console.log(`Get S3 response ::: ${JSON.stringify(response)}`);
+	return response;
 }
 
-export async function upload_file(
-  bucket_name,
-  key,
-  file_content,
-  content_type,
-) {
-  const params = {
-    Bucket: bucket_name,
-    Key: key,
-    Body: file_content,
-    ContentType: content_type,
-  };
+export async function upload_file(bucket_name, key, file_content, content_type) {
+	const params = {
+		Bucket: bucket_name,
+		Key: key,
+		Body: file_content,
+		ContentType: content_type,
+	};
 
-  const command = new PutObjectCommand(params);
-  return await S3_Client.send(command);
+	const command = new PutObjectCommand(params);
+	return await S3_Client.send(command);
 }
 
-export const upload_directory = async (
-  bucket_name,
-  local_dir_path,
-  s3_dir_path,
-) => {
-  // console.log(`Uploading directory: '${local_dir_path}' to S3 bucket ${bucket_name} > bucket directory: ${s3_dir_path}`);
-  const report_dir = fs.readdirSync(local_dir_path);
+export const upload_directory = async (bucket_name, local_dir_path, s3_dir_path) => {
+	console.log(`Uploading directory: '${local_dir_path}' to S3 bucket ${bucket_name} > bucket directory: ${s3_dir_path}`);
+	const report_dir = fs.readdirSync(local_dir_path);
 
-  for (const r_item of report_dir) {
-    const r_item_path = path.join(local_dir_path, r_item);
-    const stats = fs.statSync(r_item_path);
+	for (const r_item of report_dir) {
+		const r_item_path = path.join(local_dir_path, r_item);
+		const stats = fs.statSync(r_item_path);
 
-    if (stats.isFile()) {
-      const file_content = fs.readFileSync(r_item_path);
-      const content_type = is_zip_file_by_extension(r_item)
-        ? "application/zip"
-        : "application/octet-stream";
-      await upload_file(
-        bucket_name,
-        `${s3_dir_path}/${r_item}`,
-        file_content,
-        content_type,
-      );
-    } else if (stats.isDirectory()) {
-      await upload_directory(
-        bucket_name,
-        r_item_path,
-        `${s3_dir_path}/${r_item}`,
-      );
-    }
-  }
+		if (stats.isFile()) {
+			const file_content = fs.readFileSync(r_item_path);
+			const content_type = is_zip_file_by_extension(r_item)
+				? "application/zip"
+				: "application/octet-stream";
+			await upload_file(bucket_name, `${s3_dir_path}/${r_item}`, file_content, content_type);
+		} else if (stats.isDirectory()) {
+			await upload_directory(bucket_name, r_item_path, `${s3_dir_path}/${r_item}`);
+		}
+	}
 };
 
 function is_zip_file_by_extension(filePath) {
-  return path.extname(filePath).toLowerCase() === ".zip";
+	return path.extname(filePath).toLowerCase() === ".zip";
 }
--- e2e/client/utils/runner.js
+++ e2e/client/utils/runner.js
@@ -1,52 +1,22 @@
 import "dotenv/config";
-import CI from "is-ci";
-import os from "os";
-import fs from "fs";
-import { execSync } from "child_process";
-import { get_est_date_time, spawn_child_process, upload_directory } from "./index.js";
+import {
+	gh_actions_debug_mode,
+	get_test_reports_dir,
+	spawn_child_process,
+	upload_report,
+} from "./index.js";
 
-const { AWS_SDET_BUCKET_NAME, ENVIRONMENT: environment, APP } = process.env;
-const app = APP ?? "loan";
-const test_script_name = process.argv[2];
-const test_protocol = test_script_name.split(":")[0];
+const test_suite = process.argv[2] ?? "";
+const gh_actions_debug = gh_actions_debug_mode();
+const full_test_reports_dir = get_test_reports_dir();
 
-const test_reports_dir = "test_reports";
-const report_dir = `${get_est_date_time()}`;
-const local_test_reports_dir = `./${test_reports_dir}/${report_dir}`;
-process.env["TEST_REPORTS_DIR"] = local_test_reports_dir;
-const s3_test_reports_dir = `trading-apps/${test_reports_dir}/${app}/${environment}/${test_protocol}/${report_dir}`;
-
-const os_username = os.userInfo().username;
-const git_branch = execSync("git rev-parse --abbrev-ref HEAD").toString().trim();
-const json_report = `${local_test_reports_dir}/report.json`; // Read the report.json file to upload
-
-const upload_report = async (code) => {
-	const report_card = JSON.parse(fs.readFileSync(json_report, "utf-8"));
-
-	// Add the git-branch & username to the report_card object
-	report_card["stats"]["git_branch"] = git_branch;
-	report_card["stats"]["username"] = os_username;
-	report_card["stats"]["environment"] = environment;
-	report_card["stats"]["app"] = app;
-	report_card["stats"]["test_suite"] = test_script_name;
-
-	if (CI) {
-		const run_id = process.env.GITHUB_RUN_ID; // e.g., 1234567890
-		const repo = process.env.GITHUB_REPOSITORY; // e.g., owner/repo
-		const server = process.env.GITHUB_SERVER_URL || "https://github.com";
-		const run_url = run_id && repo ? `${server}/${repo}/actions/runs/${run_id}` : undefined;
-		const sdet = process.env.GITHUB_ACTOR || os_username; // Use GITHUB_ACTOR or fallback to os_username
-
-		report_card["ci"] = { run_id, run_url, sdet };
-	}
-
-	// Write the updated report_card object back to the report.json file
-	fs.writeFileSync(json_report, JSON.stringify(report_card, null, 2));
-
-	if(!CI) return process.exit(code ?? 1);
-	await upload_directory(AWS_SDET_BUCKET_NAME, local_test_reports_dir, s3_test_reports_dir);
-	process.exit(code ?? 1);
+const upload_report_directory = async (code) => {
+	await upload_report(code, { test_suite, full_test_reports_dir });
 };
 
-const command = `npx playwright test --project=${test_script_name}`
-spawn_child_process(command, upload_report);
+const log_level = gh_actions_debug ? "LOG_LEVEL=trace" : "LOG_LEVEL=info";
+const command = log_level
+	? `${log_level} npx playwright test --project=${test_suite}`
+	: `npx playwright test --project=${test_suite}`;
+
+spawn_child_process(command, upload_report_directory);
