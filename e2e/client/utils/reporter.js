263c48c0abf1677e3b3dd498a90295fb9ae05eff
Imran Sahil <imdshizzle@gmail.com>
2026-01-04 22:38:57 -0500

    newly extracted add_stats_to_json function

--- e2e/client/utils/reporter.js
+++ e2e/client/utils/reporter.js
@@ -12,55 +12,75 @@ import {
 	get_github_repository,
 	get_github_server_url,
 	get_github_actor,
-	artillery_record_mode
+	artillery_record_mode,
 } from "./env_loader.js";
 
-const delimeter = test_suite => {
+const delimeter = (test_suite) => {
 	return test_suite.includes(":") ? ":" : "_";
-}
+};
 
-export const upload_report = async (code, { test_suite, json_report, full_test_reports_dir, runner }) => {
+export const upload_report = async (code, { test_suite, full_test_reports_dir }) => {
 	const test_reports_dir = "test_reports";
 	const environment = get_environment();
 	const product = get_product();
-	const app_name = get_app_name();
 	const aws_sdet_bucket_name = get_aws_sdet_bucket_name();
 	const record = artillery_record_mode();
 	// Changing the report pattern can break report cards feature
 	const test_protocol = test_suite.split(delimeter(test_suite))[0];
 	const report_dir = full_test_reports_dir.split("/").pop();
 	const s3_test_reports_dir = `trading-apps/${test_reports_dir}/${product}/${environment}/${test_protocol}/${report_dir}`;
-	// json_report = json_report ?? `${full_test_reports_dir}/report.json`;
-	// const report_card = JSON.parse(fs.readFileSync(json_report, "utf-8"));
-
-	// // Add meta data below
-	// const os_username = os.userInfo().username;
-	// const git_branch = execSync("git rev-parse --abbrev-ref HEAD").toString().trim();
-	// report_card["stats"] ??= {};
-	// report_card["stats"]["app"] = product;
-	// report_card["stats"]["app_name"] = app_name;
-	// report_card["stats"]["environment"] = environment;
-	// report_card["stats"]["git_branch"] = git_branch;
-	// report_card["stats"]["protocol"] = test_protocol;
-	// report_card["stats"]["product"] = product;
-	// report_card["stats"]["runner"] = runner;
-	// report_card["stats"]["test_reports_dir"] = test_reports_dir;
-	// report_card["stats"]["test_suite"] = test_suite;
-	// report_card["stats"]["username"] = os_username;
-
-	// if (is_ci) {
-	// 	const run_id = get_github_run_id(); // e.g., 1234567890
-	// 	const repo = get_github_repository(); // e.g., owner/repo
-	// 	const server = get_github_server_url() || "https://github.com";
-	// 	const run_url = run_id && repo ? `${server}/${repo}/actions/runs/${run_id}` : "na";
-	// 	const sdet = get_github_actor() || os_username;
-	// 	report_card["ci"] = { run_id, run_url, sdet };
-	// }
-
-	// // Write the updated report_card object back to the report.json file
-	// fs.writeFileSync(json_report, JSON.stringify(report_card, null, 2));
 
 	if (!is_ci && !record) return process.exit(code ?? 1);
 	await upload_directory(aws_sdet_bucket_name, full_test_reports_dir, s3_test_reports_dir);
 	process.exit(code ?? 1);
+};
+
+export const add_stats_to_json = ({ test_suite, json_report_path, runner }) => {
+	const report_card = JSON.parse(fs.readFileSync(json_report_path, "utf-8"));
+
+	const product = get_product();
+	const app_name = get_app_name();
+	const environment = get_environment();
+	const test_protocol =
+		test_suite.split("/").pop()?.replace(".yml", "").replace(".yaml", "") || "na";
+	const test_reports_dir = "test_reports";
+
+	// Add meta data below
+	const os_username = os.userInfo().username;
+	const git_branch = execSync("git rev-parse --abbrev-ref HEAD").toString().trim();
+	report_card["stats"] ??= {};
+	report_card["stats"]["app"] = product;
+	report_card["stats"]["app_name"] = app_name;
+	report_card["stats"]["environment"] = environment;
+	report_card["stats"]["git_branch"] = git_branch;
+	report_card["stats"]["protocol"] = test_protocol;
+	report_card["stats"]["product"] = product;
+	report_card["stats"]["runner"] = runner;
+	report_card["stats"]["test_reports_dir"] = test_reports_dir;
+	report_card["stats"]["test_suite"] = test_suite;
+	report_card["stats"]["username"] = os_username;
+
+	if (is_ci) {
+		const run_id = get_github_run_id(); // e.g., 1234567890
+		const repo = get_github_repository(); // e.g., owner/repo
+		const server = get_github_server_url() || "https://github.com";
+		const run_url = run_id && repo ? `${server}/${repo}/actions/runs/${run_id}` : "na";
+		const sdet = get_github_actor() || os_username;
+		report_card["ci"] = { run_id, run_url, sdet };
+	}
+
+	// Write the updated report_card object back to the report.json file
+	fs.writeFileSync(json_report_path, JSON.stringify(report_card, null, 2));
+};
+
+export const add_stats_and_upload_report = async (
+	code,
+	{ test_suite, full_test_reports_dir, runner }
+) => {
+	const json_report_path = `${full_test_reports_dir}/report.json`;
+	add_stats_to_json({ test_suite, json_report_path, runner });
+	await upload_report(code, {
+		test_suite,
+		full_test_reports_dir,
+	});
 };
--- e2e/perf/runner.js
+++ e2e/perf/runner.js
@@ -1,118 +1,84 @@
 import is_ci from "is-ci";
-import os from "os";
-import fs from "fs";
-import { execSync } from "child_process";
 import {
 	spawn_child_process,
 	get_artillery_cloud_api_key,
 	artillery_record_mode,
-	get_environment,
-	get_app_name,
-	get_product,
-	get_aws_sdet_bucket_name,
-	get_github_run_id,
-	get_github_repository,
-	get_github_server_url,
+	add_stats_to_json,
 	get_github_actor,
 	get_est_date_time,
 	get_test_reports_dir,
 	gh_actions_debug_mode,
 	upload_report,
+	ensure_dir,
 } from "../client/utils/index.js";
 
-const test_name = process.argv[2];
+const test_suite = process.argv[2];
 const environment = process.argv[3];
 const options = process.argv[4] || "{}";
 
-const product = get_product();
-const app_name = get_app_name();
-const test_protocol = test_name.split("/").pop()?.replace(".yml", "").replace(".yaml", "") || "na";
-const test_suite = `perf:${test_protocol}`;
-const test_reports_dir = "test_reports";
-const runner = "perf/runner.js";
-
 const full_test_reports_dir = get_test_reports_dir();
-let json_report_path = "", script = "";
+let json_report_path = "";
 
 const upload_report_to_s3 = async (code) => {
-	await upload_report(code, {
-		test_suite: `perf:${script}`,
-		json_report: json_report_path,
-		full_test_reports_dir,
-	});
-};
-
-const generate_report = () => {
-	add_stats_to_json();
-	console.log(`Generating HTML report from JSON: ${json_report_path} -> ${full_test_reports_dir}`);
-	spawn_child_process(`npm run generate-haiku-report ${json_report_path} ${full_test_reports_dir}`, upload_report_to_s3);
-};
-
-const add_stats_to_json = () => {
-	const report_card = JSON.parse(fs.readFileSync(json_report_path, "utf-8"));
-
-	// Add meta data below
-	const os_username = os.userInfo().username;
-	const git_branch = execSync("git rev-parse --abbrev-ref HEAD").toString().trim();
-	report_card["stats"] ??= {};
-	report_card["stats"]["app"] = product;
-	report_card["stats"]["app_name"] = app_name;
-	report_card["stats"]["environment"] = environment;
-	report_card["stats"]["git_branch"] = git_branch;
-	report_card["stats"]["protocol"] = test_protocol;
-	report_card["stats"]["product"] = product;
-	report_card["stats"]["runner"] = runner;
-	report_card["stats"]["test_reports_dir"] = test_reports_dir;
-	report_card["stats"]["test_suite"] = test_suite;
-	report_card["stats"]["username"] = os_username;
-
-	if (is_ci) {
-		const run_id = get_github_run_id(); // e.g., 1234567890
-		const repo = get_github_repository(); // e.g., owner/repo
-		const server = get_github_server_url() || "https://github.com";
-		const run_url = run_id && repo ? `${server}/${repo}/actions/runs/${run_id}` : "na";
-		const sdet = get_github_actor() || os_username;
-		report_card["ci"] = { run_id, run_url, sdet };
+	try {
+		await upload_report(code, {
+			test_suite,
+			full_test_reports_dir,
+		});
+		process.exit(code || 0); // Exit with test code or 0
+	} catch (error) {
+		console.error(`Error uploading report: ${error.message}`);
+		process.exit(1);
 	}
+};
 
-	// Write the updated report_card object back to the report.json file
-	fs.writeFileSync(json_report_path, JSON.stringify(report_card, null, 2));
-}
+const generate_reports = (code) => {
+	console.log(`Received test completion code from the last process: ${code}`);
+	add_stats_to_json({ test_suite, json_report_path, runner: "artillery" });
+	console.log(
+		`Generating HTML report from JSON: ${json_report_path} -> ${full_test_reports_dir}`
+	);
 
-export const execute_runner = async ({ test_name, environment, tags }) => {
+	spawn_child_process(
+		`npm run generate-haiku-report ${json_report_path} ${full_test_reports_dir}`,
+		upload_report_to_s3
+	);
+};
+
+export const execute_runner = async ({ test_suite, environment, tags }) => {
 	const artillery_cloud_api_key = get_artillery_cloud_api_key();
 	const record = artillery_record_mode();
 	const actor = get_github_actor();
 
-	if (!test_name || !environment) {
+	if (!test_suite || !environment) {
 		throw new Error(`Required script arguments are missing! [${get_est_date_time()}]`);
 	}
 
 	console.log(
-		`Starting... artillery test: [${test_name}] | environment: ${environment} | options: ${options ?? "{}"} | record-mode: ${record} [${get_est_date_time()}]`
+		`Starting... artillery test: [${test_suite}] | environment: ${environment} | options: ${options ?? "{}"} | record-mode: ${record} [${get_est_date_time()}]`
 	);
-	script = test_name.split("/").pop()?.replace(".yml", "").replace(".yaml", "") || "na";
-	json_report_path = `${full_test_reports_dir}/${script}_report.json`; // e.g., ./test_reports/1-2-2026_3-42-21_PM/cards_report.json
-	
+	const feature = test_suite.split("/").pop()?.replace(".yml", "").replace(".yaml", "") || "na";
+	json_report_path = `${full_test_reports_dir}/${feature}_report.json`; // e.g., ./test_reports/1-2-2026_3-42-21_PM/cards_report.json
+
 	let artillery_tags = tags ? `,${tags}` : ""; // Tags help with filtering & reporting in Artillery Cloud
 	artillery_tags = is_ci
-		? `--tags ci:true,env:${environment},actor:${actor},dev:imran,component:${script}${artillery_tags}`
+		? `--tags ci:true,env:${environment},actor:${actor},dev:imran,component:${feature}${artillery_tags}`
 		: `--tags actor:${actor}${artillery_tags}`;
 
 	const artillery_record = record ? ` --record --key ${artillery_cloud_api_key}` : "";
 
 	const debug_mode = gh_actions_debug_mode()
-		? script == "cards"
+		? feature == "cards"
 			? "DEBUG=http* "
 			: "DEBUG=socketio "
 		: ""; // Enable Artillery debug mode for GitHub Actions runs when GH Actions debug is enabled. Will not impact LOG_LEVEL settings for processors.
 
-	const command = `${debug_mode}npx artillery run --output ${json_report_path} ${test_name} -e ${environment} ${artillery_tags}${artillery_record}`;
+	const command = `${debug_mode}npx artillery run --output ${json_report_path} ${test_suite} -e ${environment} --variables '{"test_reports_dir":"${full_test_reports_dir}"}' ${artillery_tags}${artillery_record}`;
 
-	spawn_child_process(command, generate_report);
+	spawn_child_process(command, generate_reports);
 };
 
-execute_runner({ test_name, environment }).catch((error) => {
+execute_runner({ test_suite, environment }).catch((error) => {
 	console.error(`Error executing performance test runner: ${error.message}`);
 	process.exit(1);
 });
